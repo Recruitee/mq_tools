1e715767cf8c2fcce50fb70195d4ee2cb2943a85
Marcin Stecki <madsheeppl@gmail.com>
2017-11-16 14:27:41 +0100

    dont load if there is no config

--- lib/mq_tools.ex
+++ lib/mq_tools.ex
@@ -1,3 +1,4 @@
+require Logger
 defmodule MQTools do
   import Supervisor.Spec, only: [
     worker: 2,
@@ -5,23 +6,23 @@ defmodule MQTools do
   ]
 
   def start(_, _) do
-
-    conn_opts = case Application.fetch_env(:mq_tools, :connection) do
-      {:ok, opts} -> opts
-      :error -> raise "Missing config option: mq_tools.connection"
+    conn_opts = Application.get_env(:mq_tools, :connection)
+    children = if conn_opts do
+      [
+        worker(MQTools.AMQPConnection, [conn_opts]),
+        worker(MQTools.Client.Requests, []),
+        worker(MQTools.Client, []),
+        worker(MQTools.Provider.Dispatcher, []),
+        supervisor(MQTools.Provider.HandlerSupervisor, [])
+      ]
+    else
+      Logger.info "MQTools did not start providers and client, missing config option: mq_tools.connection"
+      []
     end
 
     # this needs to be revised, as we probably don't wanna restart requests on client failure etc.
     # Might break concurent requests when one of them fail.
-
-    Supervisor.start_link([
-      worker(MQTools.AMQPConnection, [conn_opts]),
-      worker(MQTools.Client.Requests, []),
-      worker(MQTools.Client, []),
-      worker(MQTools.Provider.Dispatcher, []),
-      supervisor(MQTools.Provider.HandlerSupervisor, [])
-    ], strategy: :one_for_all, name: __MODULE__)
-
+    Supervisor.start_link(children, strategy: :one_for_all, name: __MODULE__)
   end
 
   def amqp_connection do
